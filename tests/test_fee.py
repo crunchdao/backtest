7df8ae6dd30c7a285ecf5882c7473379e9d3c696
Caceresenzo <caceresenzo1502@gmail.com>
2022-07-07 17:48:22 +0200

    test(fee): added interactive brokers formula

--- tests/test_fee.py
+++ tests/test_fee.py
@@ -25,3 +25,12 @@ class ExpressionFeeModelTest(unittest.TestCase):
         order = backtest.Order("AAPL", 15, 2)
 
         self.assertEqual(5 * order.price * order.quantity, model.get_order_fee(order))
+
+    def test_get_order_fee_interactive_broker(self):
+        model = backtest.fee.ExpressionFeeModel("max(abs(price * quantity) * 0.01, 1)")
+        
+        order = backtest.Order("AAPL", 1, 20)
+        self.assertEqual(1, model.get_order_fee(order))
+        
+        order = backtest.Order("AAPL", 150, 20)
+        self.assertEqual(30, model.get_order_fee(order))
