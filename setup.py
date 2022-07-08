f07ecff0dd3e1a9f79aaa111d4a425609f74a4fe
Caceresenzo <caceresenzo1502@gmail.com>
2022-07-08 22:26:17 +0200

    fix(setup): was not finding nested directories

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='datacrunch-backtest',
-    version='1.0',
+    version='1.0.0',
     description='Simple backtester',
     author='Enzo CACERES',
     author_email='caceresenzo1502@gmail.com',
+    packages=find_packages()
 )
