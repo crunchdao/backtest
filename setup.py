2697db6793b67c7f32939bba3cf243514d4639d1
Caceresenzo <caceresenzo1502@gmail.com>
2022-07-08 23:02:57 +0200

    fix(setup): added requirements

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@
 
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as fd:
+    requirements = fd.read().splitlines()
+
 setup(
     name='datacrunch-backtest',
     version='1.0.0',
     description='Simple backtester',
     author='Enzo CACERES',
     author_email='caceresenzo1502@gmail.com',
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=requirements,
 )
