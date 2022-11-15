5ff36a15330a5b02f16dc95b5b280c9f31277a12
Caceresenzo <caceresenzo1502@gmail.com>
2022-11-15 15:52:59 +0100

    fix(export/dump): holes were causing problem

--- backtest/export/dump.py
+++ backtest/export/dump.py
@@ -1,6 +1,9 @@
 import abc
 import os
 import sys
+import math
+import typing
+import datetime
 
 from ..utils import signum
 
@@ -21,62 +24,86 @@ class DumpExporter(BaseExporter):
         self.output_file = output_file
         self.auto_delete = auto_delete
         self.auto_override = auto_override
-        
-        self.dataframe = pandas.DataFrame(columns=["date", "symbol", "quantity", "price", "market_price", "equity"])
-        
+        self.all_dates = set()
+
+        self.dataframe = pandas.DataFrame(
+            columns=["date", "symbol", "quantity", "price", "market_price", "equity"])
+
     @abc.abstractmethod
     def initialize(self) -> None:
         if self.auto_override:
             return
-        
+
         for file in [self.output_file]:
             if file is None or not os.path.exists(file):
                 continue
-            
+
             can_delete = self.auto_delete
             if not can_delete:
-                can_delete = input(f"{file}: delete file? [y/N]").lower() == 'y'
-            
+                can_delete = input(
+                    f"{file}: delete file? [y/N]").lower() == 'y'
+
             if can_delete:
                 os.remove(file)
 
     @abc.abstractmethod
     def on_snapshot(self, snapshot: Snapshot) -> None:
         date = snapshot.date
-        
+        self.all_dates.add(date)
+
         if snapshot.postponned is not None:
             date = snapshot.postponned
-        
+
         self.dataframe = pandas.concat([
             self.dataframe,
             pandas.DataFrame(
                 [
-                    [date, holding.symbol, holding.quantity, holding.price, holding.market_price, snapshot.equity]
+                    [date, holding.symbol, holding.quantity, holding.price,
+                        holding.market_price, snapshot.equity]
                     for holding in snapshot.holdings
                 ],
                 columns=self.dataframe.columns,
             )
         ], axis=0)
 
+    def get_missing_dates(self, dates: typing.Set[datetime.date]):
+        return list(filter(
+            lambda x: x not in dates,
+            self.all_dates
+        ))
+
     @abc.abstractmethod
     def finalize(self) -> None:
         if not len(self.dataframe):
-            print("[warning] cannot create dump: dataframe is empty", file=sys.stderr)
+            print("[warning] cannot create dump: dataframe is empty",
+                  file=sys.stderr)
             return
-        
+
         def inverse_sign_if_shorting(row: pandas.Series):
             if signum(row['quantity']) == -1:
                 row['profit'] = row['profit'] * -1
-            
+
             return row
 
         def compute_profit(group: pandas.DataFrame):
+            missing = self.get_missing_dates(group["date"].unique())
+            holes = pandas.DataFrame(
+                [
+                    [date, group.name, 0, math.nan, math.nan, math.nan]
+                    for date in missing
+                ],
+                columns=["date", "symbol", "quantity", "price", "market_price", "equity"],
+            )
+
+            group = pandas.concat([group, holes])
+            group.sort_values("date", inplace=True)
+
             price_yesterday = group["price"].shift(1)
 
             group['profit'] = (group['price'] - price_yesterday) / price_yesterday
 
             return group.apply(inverse_sign_if_shorting, axis=1)
-        
+
         self.dataframe = self.dataframe.groupby("symbol").apply(compute_profit)
         self.dataframe.set_index("date", inplace=True)
 
@@ -84,4 +111,5 @@ class DumpExporter(BaseExporter):
             if self.auto_override or not os.path.exists(self.output_file):
                 self.dataframe.to_csv(self.output_file)
             else:
-                print(f"[warning] {self.output_file} already exists", file=sys.stderr)
+                print(
+                    f"[warning] {self.output_file} already exists", file=sys.stderr)
