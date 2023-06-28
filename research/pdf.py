6672cb3b575e3cd15be0a1952451f2ecd63ae617
Caceresenzo <caceresenzo1502@gmail.com>
2023-06-28 19:32:43 +0200

    refactor(research/pdf): use more functions

--- research/pdf.py
+++ research/pdf.py
@@ -60,10 +60,6 @@ class Color:
     alpha: int
 
     @property
-    def rbg(self) -> typing.Tuple[int, int, int]:
-        return (self.red, self.green, self.blue)
-
-    @property
     def hex_string(self) -> typing.Tuple[int, int, int]:
         return '#%02x%02x%02x%02x' % (
             int(self.red),
@@ -127,9 +123,9 @@ class Document:
     pages: typing.List[Page]
 
     @property
-    def font_families(self) -> typing.List[str]:
-        return set(
-            element.font.name
+    def fonts(self) -> typing.List[Font]:
+        return list(
+            element.font
             for page in self.pages
             for element in page.elements
             if isinstance(element, Text)
@@ -183,12 +179,6 @@ class TemplateLoader:
         raise NotImplemented()
 
 
-class TemplateLoader:
-
-    def load(self, path: str) -> Template:
-        raise NotImplemented()
-
-
 class TemplateRenderer:
 
     def render(self, template: Template, output: io.FileIO) -> Template:
@@ -203,18 +193,9 @@ class SketchTemplateLoader(TemplateLoader):
         def find(layer: dict, absolute_offset: Vector2, clip: Rectangle2):
             id = layer["do_objectID"]
             natural_id = layer["name"]
-            class_ = layer["_class"]
 
-            size = Vector2(
-                layer["frame"]["width"],
-                layer["frame"]["height"]
-            )
-
-            local = Vector2(
-                layer["frame"]["x"],
-                layer["frame"]["y"],
-            )
-
+            local = self._get_frame_xy(layer)
+            size = self._get_frame_wh(layer)
             absolute = local + absolute_offset
 
             position = Rectangle2(
@@ -222,90 +203,68 @@ class SketchTemplateLoader(TemplateLoader):
                 size.x, size.y,
             )
 
-            if class_ == "shapePath":
-                fill = next(iter(layer["style"]["fills"]), None)
+            class_ = layer["_class"]
+            match class_:
+                case "shapePath":
+                    (
+                        points,
+                        color
+                    ) = self._extract_shape_points(
+                        layer,
+                        absolute,
+                        size
+                    )
 
-                color = Color(
-                    fill["color"]["red"] * 255,
-                    fill["color"]["green"] * 255,
-                    fill["color"]["blue"] * 255,
-                    fill["color"]["alpha"] * 255
-                ) if fill else Color.black()
-
-                points: typing.List[Vector2] = []
-                for point in layer["points"]:
-                    x, y = tuple(map(float, point["curveTo"].replace(
-                        "{", "").replace("}", "").split(", ")))
-                    points.append(Vector2(
-                        absolute.x + x * size.x,
-                        absolute.y + y * size.y,
+                    elements.append(Shape(
+                        id=id,
+                        natural_id=natural_id,
+                        position=position,
+                        points=points,
+                        color=color,
+                        clip=clip,
                     ))
 
-                elements.append(Shape(
-                    id=id,
-                    natural_id=natural_id,
-                    position=position,
-                    points=points,
-                    color=color,
-                    clip=clip,
-                ))
-                print(id, clip)
+                case "text":
+                    (
+                        content,
+                        font,
+                        color
+                    ) = self._extract_text(
+                        layer
+                    )
 
-            if class_ == "text":
-                content = layer["attributedString"]["string"]
-                content = content.replace("\uFB01", "fi")
-                content = content.replace("\uFB02", "fl")
-                content = content.replace("\t", " ")
-                content = content.replace("\n", " ")
+                    elements.append(Text(
+                        id=id,
+                        natural_id=natural_id,
+                        position=position,
+                        content=content,
+                        font=font,
+                        color=color,
+                    ))
 
-                font = Font(
-                    family=layer["style"]["textStyle"]["encodedAttributes"]["MSAttributedStringFontAttribute"]["attributes"]["name"],
-                    size=layer["style"]["textStyle"]["encodedAttributes"]["MSAttributedStringFontAttribute"]["attributes"]["size"]
-                )
+                case "bitmap":
+                    path = layer["image"]["_ref"]
+                    path = os.path.join("tearsheet", path)
 
-                color = Color(
-                    layer["style"]["textStyle"]["encodedAttributes"]["MSAttributedStringColorAttribute"]["red"] * 255,
-                    layer["style"]["textStyle"]["encodedAttributes"]["MSAttributedStringColorAttribute"]["green"] * 255,
-                    layer["style"]["textStyle"]["encodedAttributes"]["MSAttributedStringColorAttribute"]["blue"] * 255,
-                    layer["style"]["textStyle"]["encodedAttributes"]["MSAttributedStringColorAttribute"]["alpha"] * 255,
-                )
-
-                elements.append(Text(
-                    id=id,
-                    natural_id=natural_id,
-                    position=position,
-                    content=content,
-                    font=font,
-                    color=color,
-                ))
-
-            if class_ == "bitmap":
-                path = layer["image"]["_ref"]
-                path = os.path.join("tearsheet", path)
-
-                elements.append(Image(
-                    id=id,
-                    natural_id=natural_id,
-                    position=position,
-                    path=path,
-                ))
+                    elements.append(Image(
+                        id=id,
+                        natural_id=natural_id,
+                        position=position,
+                        path=path,
+                    ))
 
             clip: Rectangle2 = None
             for sub_layer in layer.get("layers", []):
                 if sub_layer["hasClippingMask"]:
-                    sub_local = Vector2(
-                        sub_layer["frame"]["x"],
-                        sub_layer["frame"]["y"],
-                    )
+                    sub_size = self._get_frame_wh(sub_layer)
+                    sub_local = self._get_frame_xy(sub_layer)
 
                     sub_absolute = sub_local + absolute
 
                     clip = Rectangle2(
                         sub_absolute.x, sub_absolute.y,
-                        sub_layer["frame"]["width"],
-                        sub_layer["frame"]["height"],
+                        sub_size.x, sub_size.y,
                     )
-                    print("new clip", sub_layer["do_objectID"], clip)
                 else:
                     find(sub_layer, absolute, clip)
                     clip = None
@@ -319,10 +278,7 @@ class SketchTemplateLoader(TemplateLoader):
             find(layer, Vector2.zero(), None)
 
             pages.append(Page(
-                size=Vector2(
-                    layer["frame"]["width"],
-                    layer["frame"]["height"],
-                ),
+                size=self._get_frame_wh(layer),
                 elements=elements
             ))
 
@@ -334,6 +290,77 @@ class SketchTemplateLoader(TemplateLoader):
             str(sketch["do_objectID"])[:50],
             document
         )
+
+    def _get_frame_xy(self, layer: dict):
+        return Vector2(
+            layer["frame"]["x"],
+            layer["frame"]["y"],
+        )
+
+    def _get_frame_wh(self, layer: dict):
+        return Vector2(
+            layer["frame"]["width"],
+            layer["frame"]["height"],
+        )
+
+    def _extract_xy(self, input: str):
+        input = input.replace("{", "")
+        input = input.replace("}", "")
+
+        return tuple(map(float, input.split(", ")))
+
+    def _convert_color(self, input: dict) -> Color:
+        return Color(
+            input["red"] * 255,
+            input["green"] * 255,
+            input["blue"] * 255,
+            input["alpha"] * 255
+        )
+
+    def _extract_shape_points(
+        self,
+        layer: dict,
+        absolute: Vector2,
+        size: Vector2
+    ) -> Shape:
+        fill = next(iter(layer["style"]["fills"]), None)
+        color = self._convert_color(fill["color"]) if fill else Color.black()
+
+        points: typing.List[Vector2] = []
+        for point in layer["points"]:
+            x, y = self._extract_xy(point["curveTo"])
+
+            points.append(Vector2(
+                absolute.x + x * size.x,
+                absolute.y + y * size.y,
+            ))
+
+        return points, color
+
+    def _extract_text(
+        self,
+        layer: dict,
+    ) -> Shape:
+        content = layer["attributedString"]["string"]
+        content = content.replace("\uFB01", "fi")
+        content = content.replace("\uFB02", "fl")
+        content = content.replace("\t", " ")
+        content = content.replace("\n", " ")
+        content = content.replace("’", "'")
+
+        font = Font(
+            family=layer["style"]["textStyle"]["encodedAttributes"]["MSAttributedStringFontAttribute"]["attributes"]["name"],
+            size=layer["style"]["textStyle"]["encodedAttributes"]["MSAttributedStringFontAttribute"]["attributes"]["size"]
+        )
+
+        color = Color(
+            layer["style"]["textStyle"]["encodedAttributes"]["MSAttributedStringColorAttribute"]["red"] * 255,
+            layer["style"]["textStyle"]["encodedAttributes"]["MSAttributedStringColorAttribute"]["green"] * 255,
+            layer["style"]["textStyle"]["encodedAttributes"]["MSAttributedStringColorAttribute"]["blue"] * 255,
+            layer["style"]["textStyle"]["encodedAttributes"]["MSAttributedStringColorAttribute"]["alpha"] * 255,
+        )
+
+        return content, font, color
 
 
 class PdfTemplateRenderer(TemplateRenderer):
@@ -366,12 +393,15 @@ class PdfTemplateRenderer(TemplateRenderer):
             unit="pt"
         )
 
-        pdf.set_compression(False)
-        pdf.set_text_color(255, 0, 0)
-        pdf.set_fill_color(0, 255, 0)
+        for font in template.document.fonts:
+            file_name = f"{font.family}.ttf"
+            if os.path.exists(file_name):
+                if font.family.lower() not in pdf.fonts:
+                    pdf.add_font(font.family, "", file_name)
+            else:
+                font.family = "helvetica"
+
         pdf.set_auto_page_break(False)
-        pdf.add_font("LucidaGrande", '', "Lucida Grande Regular.ttf")
-        pdf.add_font("LucidaGrande-Bold", '', "LucidaGrandeBold.ttf")
 
         for page in tqdm.tqdm(template.document.pages, desc="page"):
             pdf.add_page(
@@ -389,67 +419,75 @@ class PdfTemplateRenderer(TemplateRenderer):
                 pdf.set_xy(element.position.x, element.position.y)
 
                 if isinstance(element, Shape):
-                    shape = element
-
-                    with pdf.new_path(shape.position.x, shape.position.y) as path:
-                        path.style.fill_color = shape.color.hex_string
-                        path.style.stroke_color = fpdf.drawing.gray8(0, 0)
-                        path.style.stroke_opacity = 0
-
-                        clip = shape.clip
-                        if clip:
-                            clipping_path = fpdf.drawing.ClippingPath()
-                            clipping_path.rectangle(
-                                clip.x, clip.y,
-                                clip.width, clip.height,
-                            )
-
-                            path.clipping_path = clipping_path
-
-                        for point in shape.points:
-                            path.line_to(point.x, point.y)
-
-                        path.close()
-
-                if isinstance(element, Text):
-                    text = element
-
-                    font = text.font
-                    if font.family.lower() in pdf.fonts:
-                        pdf.set_font(font.family, '', font.size)
-                    else:
-                        pdf.set_font("Arial", '', font.size)
-                        print("unknown", font.family)
-
-                    pdf.set_xy(pdf.x - 3, pdf.y + 1)
-                    pdf.set_text_color(text.color.rbg)
-                    pdf.cell(text.position.width,
-                             text.position.height, text.content)
-
+                    self._render_shape(pdf, element)
+                elif isinstance(element, Text):
+                    self._render_text(pdf, element)
                 elif isinstance(element, Image):
-                    image = element
-
-                    pdf.image(
-                        image.path,
-                        image.position.x, image.position.y,
-                        image.position.width, image.position.height
-                    )
+                    self._render_image(pdf, element)
 
             if self.debug:
-                for element in elements:
-                    if isinstance(element, Text):
-                        pdf.set_draw_color(255, 0, 0)
-                    elif isinstance(element, Image):
-                        pdf.set_draw_color(0, 255, 0)
-                    elif isinstance(element, Shape):
-                        pdf.set_draw_color(0, 0, 255)
-                    else:
-                        continue
-
-                    pdf.rect(element.position.x, element.position.y,
-                             element.position.width, element.position.height)
+                self._render_debug(pdf, elements)
 
         pdf.output(output)
+
+    def _render_image(self, pdf: fpdf.FPDF, image: Image):
+        pdf.image(
+            image.path,
+            image.position.x, image.position.y,
+            image.position.width, image.position.height
+        )
+
+    def _render_text(self, pdf: fpdf.FPDF, text: Text):
+        font = text.font
+        pdf.set_font(font.family, '', font.size)
+
+        pdf.set_xy(pdf.x - 3, pdf.y + 1)
+        pdf.set_text_color(text.color.red, text.color.green, text.color.blue)
+
+        pdf.cell(
+            text.position.width,
+            text.position.height,
+            text.content
+        )
+
+    def _render_shape(self, pdf: fpdf.FPDF, shape: Shape):
+        with pdf.new_path(shape.position.x, shape.position.y) as path:
+            path.style.fill_color = shape.color.hex_string
+            path.style.stroke_color = fpdf.drawing.gray8(0, 0)
+            path.style.stroke_opacity = 0
+
+            clip = shape.clip
+            if clip:
+                clipping_path = fpdf.drawing.ClippingPath()
+                clipping_path.rectangle(
+                    clip.x, clip.y,
+                    clip.width, clip.height,
+                )
+
+                path.clipping_path = clipping_path
+
+            for point in shape.points:
+                path.line_to(point.x, point.y)
+
+            path.close()
+
+    def _render_debug(self, pdf: fpdf.FPDF, elements: typing.List[Element]):
+        for element in elements:
+            if isinstance(element, Text):
+                pdf.set_draw_color(255, 0, 0)
+            elif isinstance(element, Image):
+                pdf.set_draw_color(0, 255, 0)
+            elif isinstance(element, Shape):
+                pdf.set_draw_color(0, 0, 255)
+            else:
+                continue
+
+            pdf.rect(
+                element.position.x,
+                element.position.y,
+                element.position.width,
+                element.position.height
+            )
 
 
 print()
