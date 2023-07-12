f578b07a4e46f26ce8e34ad9df6bf55d8316df27
Caceresenzo <caceresenzo1502@gmail.com>
2023-07-12 19:23:52 +0200

    feat(template): add regex support

--- backtest/template/template.py
+++ backtest/template/template.py
@@ -1,5 +1,7 @@
 import io
 import shutil
+import re
+import collections
 
 from .models import *
 
@@ -18,29 +20,43 @@ class Template:
         self.name = name
         self.document = document
 
-        self.slots = {}
+        self.slots = collections.defaultdict(list)
         for page in document.pages:
             for element in page.elements:
                 if element.id in self.slots:
                     self.slots[element.id].append(element)
-                else:
-                    self.slots[element.id] = [element]
 
                 if element.natural_id in self.slots:
                     self.slots[element.natural_id].append(element)
-                else:
-                    self.slots[element.natural_id] = [element]
 
-    def apply(self, variables: typing.Dict[NaturalIdentifier | Identifier, typing.Any]):
+    def apply(self, variables: typing.Dict[NaturalIdentifier | Identifier, typing.Callable[[str], typing.Any] | typing.Any]):
         for key, value in variables.items():
             self.set(key, value)
 
-    def set(self, key: NaturalIdentifier | Identifier, value: typing.Any):
+    def apply_re(self, variables: typing.Dict[str, typing.Callable[[str], typing.Any] | typing.Any]):
+        for pattern, value in variables.items():
+            for key, elements in self.slots.items():
+                match = re.search(f"^{pattern}$", key)
+                if match is None:
+                    continue
+    
+                if callable(value):
+                    value = value(key, *match.groups())
+                
+                self._set(elements, value)
+
+    def set(self, key: NaturalIdentifier | Identifier | re.Pattern, value: typing.Callable[[str], typing.Any] | typing.Any):
         elements = self.slots.get(key)
         if elements is None:
             print(f"no element for key={key}")
             return
+    
+        if callable(value):
+            value = value(key)
 
+        return self._set(elements, value)
+
+    def _set(self, elements: typing.List[Element], value: typing.Any):
         for element in elements:
             if isinstance(element, Text):
                 text = element
