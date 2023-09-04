5fbf02a4115b1c5046798c88195755b86ea6fa59
matteoettam09 <matteomanzi09@gmail.com>
2023-09-04 22:06:57 +0000

    fix: quantstat version

--- backtest/export/quants.py
+++ backtest/export/quants.py
@@ -110,6 +110,6 @@ class QuantStatsExporter(BaseExporter):
 
         if self.html_output_file is not None:
             if self.auto_override or not os.path.exists(self.html_output_file):
-                quantstats.reports.html(returns, benchmark=benchmark, output=True, download_filename=self.html_output_file)
+                quantstats.reports.html(returns, benchmark=benchmark, output=self.html_output_file)
             else:
                 print(f"[warning] {self.html_output_file} already exists", file=sys.stderr)
